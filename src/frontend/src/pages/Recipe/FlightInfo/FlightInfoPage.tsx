f0bf27723cfd9e583818b1cf4bd5d96f64335ea3
fairay <48217178+fairay@users.noreply.github.com>
2023-05-31 00:35:48 +0300

    simple flight card

--- src/frontend/src/pages/Recipe/FlightInfo/FlightInfoPage.tsx
+++ src/frontend/src/pages/Recipe/FlightInfo/FlightInfoPage.tsx
@@ -3,6 +3,8 @@ import GetFlight from "postAPI/flights/Get"
 import React from "react"
 import { NavigateFunction, Params } from "react-router-dom"
 import { Flight as FlightT} from "types/Flight"
+import styles from "./FlightsInfoPage.module.scss";
+import RoundButton from "components/RoundButton/RoundButton"
 
 type State = {
     flight?: FlightT
@@ -31,16 +33,61 @@ class FlightInfoPage extends React.Component<RecipeInfoParams, State> {
         });
     }
 
+
+    airport_img() {
+        return (
+            <img
+                src="/airport.svg"
+                className={styles.airports_img}
+            />
+        )
+    }
+
+    fromBox() {
+        return (<Box >
+            {this.airport_img()}
+            <Text>{this.state.flight?.fromAirport}</Text>
+        </Box>);
+    };
+
+    toBox() {
+        return (<Box>
+            {this.airport_img()}
+            <Text>{this.state.flight?.toAirport}</Text>
+        </Box>);
+    };
+
+    submit(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        let button = e.currentTarget
+        button.disabled = true
+        // TODO: buy query here
+        // LoginQuery(this.acc).then(data => {
+        //     button.disabled = false
+        //     if (data.status === 200) {
+        //         window.location.href = '/';
+        //     } else {
+        //         var title = document.getElementById("undertitle")
+        //         if (title)
+        //             title.innerText = "Ошибка авторизации!"
+        //     }
+        // });
+    }
+
     render() {
         return (
-            <VStack>
+            <VStack className={styles.main_box}>
                 {this.state.flight &&
-                    <Box>
+                    <Box style={{width: "100%"}}>
                         <Text>Рейс номер {this.flightNumber}</Text>
-                        <Text>Аэропорт отправления: {this.state.flight?.fromAirport}</Text>
-                        <Text>Аэропорт прибытия: {this.state.flight?.toAirport}</Text>
-                        <Text>Время начала полёта: {this.state.flight?.date}</Text>
-                        <Text>Стоимость: {this.state.flight?.price}</Text>
+                        <Box className={styles.airports_box}>
+                            {this.fromBox()}
+                            <Text>{this.state.flight?.date}</Text>
+                            {this.toBox()}
+                        </Box>
+
+                        <RoundButton style={{width:"100%", marginTop: "20px"}} type="submit" onClick={event => this.submit(event)}>
+                            Забронировать билиет за {this.state.flight?.price}
+                        </RoundButton>
                     </Box>
                 }
             </VStack>
