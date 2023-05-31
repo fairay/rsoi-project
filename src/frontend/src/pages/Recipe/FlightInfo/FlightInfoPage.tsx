ee650a6e9c5ceb41c40c791c054f876fc35273b0
fairay <48217178+fairay@users.noreply.github.com>
2023-06-01 00:57:03 +0300

    added ticket creation

--- src/frontend/src/pages/Recipe/FlightInfo/FlightInfoPage.tsx
+++ src/frontend/src/pages/Recipe/FlightInfo/FlightInfoPage.tsx
@@ -1,4 +1,4 @@
-import { VStack, Text, Box } from "@chakra-ui/react"
+import { VStack, Text, Box, Switch } from "@chakra-ui/react"
 import GetFlight from "postAPI/flights/Get"
 import React from "react"
 import { NavigateFunction, Params } from "react-router-dom"
@@ -6,8 +6,11 @@ import { Flight as FlightT} from "types/Flight"
 import styles from "./FlightsInfoPage.module.scss";
 import RoundButton from "components/RoundButton/RoundButton"
 
+import CreateTicket from "postAPI/tickets/Create"
+
 type State = {
     flight?: FlightT
+    useBonusPoints: boolean
 }
 
 type RecipeInfoParams = {
@@ -21,7 +24,9 @@ class FlightInfoPage extends React.Component<RecipeInfoParams, State> {
     constructor(props) {
         super(props);
         this.flightNumber = this.props.match.flightNumber || "?";
-        this.state = {}
+        this.state = {
+            useBonusPoints: false
+        }
     };
 
     componentDidMount(): void {
@@ -58,19 +63,28 @@ class FlightInfoPage extends React.Component<RecipeInfoParams, State> {
     };
 
     submit(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        if (!this.state.flight) {
+            console.error("flight is null")
+            return
+        }
+
         let button = e.currentTarget
         button.disabled = true
-        // TODO: buy query here
-        // LoginQuery(this.acc).then(data => {
-        //     button.disabled = false
-        //     if (data.status === 200) {
-        //         window.location.href = '/';
-        //     } else {
-        //         var title = document.getElementById("undertitle")
-        //         if (title)
-        //             title.innerText = "Ошибка авторизации!"
-        //     }
-        // });
+        CreateTicket(this.state.flight.flightNumber, this.state.flight.price, this.state.useBonusPoints)
+        .then(data => {
+            button.disabled = false
+            if (data.status === 200) {
+                window.location.href = '/tickets';
+            } else {
+                var title = document.getElementById("undertitle")
+                if (title)
+                    title.innerText = "Ошибка покупки билета"
+            }
+        });
+    }
+
+    async handleToggle() {
+        this.setState({useBonusPoints: !this.state.useBonusPoints})
     }
 
     render() {
@@ -85,7 +99,20 @@ class FlightInfoPage extends React.Component<RecipeInfoParams, State> {
                             {this.toBox()}
                         </Box>
 
-                        <RoundButton style={{width:"100%", marginTop: "20px"}} type="submit" onClick={event => this.submit(event)}>
+                        <Box className={styles.bonus_div}>
+                            <Switch
+                                isChecked={this.state.useBonusPoints}
+                                onChange={() => this.handleToggle()}
+                                colorScheme="teal"
+                                size="md"
+                            />
+                            <Text>
+                                Использовать бонусные баллы
+                            </Text>
+                        </Box>
+
+
+                        <RoundButton className={styles.buy_button} type="submit" onClick={event => this.submit(event)}>
                             Забронировать билиет за {this.state.flight?.price}
                         </RoundButton>
                     </Box>
